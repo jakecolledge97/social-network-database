6e6d9cab467452a06591d5cb70bd3f3e81a6ad7d
John Conway <88188872+jakecolledge97@users.noreply.github.com>
2022-01-13 16:48:13 +1030

    Added route files, so server can run

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const db = require('./config/connection');
-const routes = require('./routes')
-
-const cwd = process.cwd();
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();

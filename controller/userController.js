b21231a15e46e1d72cf35c6c782cb5bde5b3facc
John Conway <88188872+jakecolledge97@users.noreply.github.com>
2022-01-14 19:04:40 +1030

    added add friend post route that adds friends

--- controller/userController.js
+++ controller/userController.js
@@ -1,24 +1,75 @@
 const res = require("express/lib/response");
-const { User } = require("../models")
+const { User } = require("../models");
 
 module.exports = {
-    //get ALL users
-    getUsers(req, res){
-        User.find()
-        .then(async(users) => {
-            const userObj = {
-                users
-            };
-            return res.json(userObj);
-        })
-        .catch((err) => {
-            console.log(err);
-            return res.status(500).json(err)
-        })
-    },
-    createUser(req,res){
-        User.create(req.body)
-        .then((user) => res.json(user))
-        .catch((err) => res.status(500).json(err))
-    },
-}
+  //finds ALL users
+  getUsers(req, res) {
+    User.find()
+      .then(async (users) => {
+        const userObj = {
+          users,
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  //creates user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  //gets single user
+  getUserById(req, res) {
+    User.findById(req.params.userId)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  //updates user
+  updateUserById(req, res) {
+    User.findByIdAndUpdate(
+      req.params.userId,
+      { $set: { username: req.body.username } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this id!" })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  //add Friend
+  async addFriend(req, res) {
+      //adds friend to userId
+    User.findByIdAndUpdate(
+      req.params.userId,
+      { $addToSet: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this id!" })
+          //adds user to friendId
+          : User.findByIdAndUpdate(
+              req.params.friendId,
+              { $addToSet: { friends: req.params.userId } },
+              { runValidators: true, new: true }
+            ).then((friend) =>
+              !friend
+                ? res.status(404).json({ message: "No user with this id!" })
+                : res.json(friend)
+            )
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,8 +2,15 @@ const router = require('express').Router();
 const {
     getUsers,
     createUser,
+    getUserById,
+    updateUserById,
+    addFriend,
 } = require('../../controller/userController')
 
 router.route('/').get(getUsers).post(createUser)
+
+router.route('/:userId').get(getUserById).put(updateUserById)
+
+router.route('/:userId/friends/:friendId').post(addFriend)
 
 module.exports = router;

4361c48ebd7c4f5c52cb95dac3959ef764de13ea
John Conway <88188872+jakecolledge97@users.noreply.github.com>
2022-01-14 11:43:21 +1030

    filled user schema and virtuals

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([a-z0-9]+)\.([a-z]{2,6})$/, 'Please enter a valid email.'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thoughts',
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            }
+        ]
+    }
+);
+
+//virtuals that returns number of friends
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
